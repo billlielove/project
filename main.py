becdda749d3aaf05becf425df0c3b85b57be9bac
billlielove <seeunirami@gmail.com>
2023-02-01 10:56:32 +0000

    commit 3

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ clock = pygame.time.Clock()
 # Title screen
 title_font = pygame.font.Font("Fonts/pixelatedfont.ttf", 50)
 title_name = title_font.render("Evolution Simulator", False, (0, 0, 0))
-title_name_rect = title_name.get_rect(center=(screen_width/2, 120))
+title_name_rect = title_name.get_rect(center=(screen_width/2, 200))
 title_surf = pygame.image.load("Backgrounds/titlebackground.png").convert()
 
 # Game loop
@@ -23,7 +23,11 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+    
+    # Title Screen
     screen.blit(title_surf,(0, 0))
     screen.blit(title_name, title_name_rect)
 
+    pygame.display.flip()
     pygame.display.update()
+    clock.tick(60)
