46c5f734863494f6ea822048fd1f4cbd7771af6e
billlielove <seeunirami@gmail.com>
2023-02-02 12:32:15 +0000

    buttons and changing screens

--- main.py
+++ main.py
@@ -19,14 +19,54 @@ title_name_rect = title_name_surf.get_rect(center=(screen_width / 2, 200))  # Co
 title_background_surf = pygame.image.load("Backgrounds/titlebackground.png").convert()  # Getting the background image and turning it into a surface
 title_screen = True
 
+# Button fonts and other variables
+button_font = pygame.font.Font("Fonts/pixelatedfont.ttf", 20)
+
+# Main screen surface
+main_background_surf = pygame.image.load("Backgrounds/mainbackground.png").convert()
+
+# Settings screen surface
+settings_background_surf = pygame.image.load("Backgrounds/settingsbackground.png").convert()
+
+class Button:
+    def __init__(self, text, width, height, pos):
+        self.clicked = False
+
+        self.body_rect = pygame.Rect(pos, (width, height))
+        self.body_colour = (145, 151, 176)
+
+        self.text_surf = button_font.render(text, False, (0, 0, 0))
+        self.text_rect = self.text_surf.get_rect(center=self.body_rect.center)
+
+    def draw(self):
+        pygame.draw.rect(screen, self.body_colour, self.body_rect, border_radius=12)
+        screen.blit(self.text_surf, self.text_rect)
+        self.check_click()
+
+    def check_click(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.body_rect.collidepoint(mouse_pos):
+            mouse_pos = pygame.mouse.get_pos()
+            if pygame.mouse.get_pressed()[0]:
+                self.clicked = True
+                return True
+            else:
+                if self.clicked:
+                    self.clicked = False
+
+# Buttons
+start_button = Button("Start", 150, 50, (200, 400))
+options_button = Button("Options", 150, 50, (450, 400))
 
 class Organism(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        crab = pygame.image.load("Crab/crab1.png").convert_alpha()
-        self.image = crab
-        self.rect = crab.get_rect(midbottom=(400, 400))
+        crab1 = pygame.image.load("Crab/crab1.png").convert_alpha()
+        self.image = crab1
+        self.rect = crab1.get_rect(center=(400, 400))
 
+
+# Crab
 crab = pygame.sprite.GroupSingle()
 crab.add(Organism())
 
@@ -41,9 +81,12 @@ while True:
     if title_screen:  # Displaying surfaces and rectangles on the screen
         screen.blit(title_background_surf, (0, 0))  # displays the title background image onto the screen
         screen.blit(title_name_surf, title_name_rect)  # displays the title name on top of the background image on the screen
-
+        start_button.draw()
+        if start_button.check_click() == True:
+            title_screen = False
+        options_button.draw()
     else:
-        crab.draw(screen)
+        screen.blit(main_background_surf, (0, 0))
 
     pygame.display.update()  # updates the display
     clock.tick(60)  # Limits the runtime of the game to 60 ticks/frames per second
