b5fe5175d536ad6c02ec8101ad4b8ae1bd9a1c30
billlielove <seeunirami@gmail.com>
2023-02-09 13:13:25 +0000

    Navigation Update

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ button_font = pygame.font.Font("Fonts/pixelatedfont.ttf", 20)
 
 # Main screen surface
 main_background_surf = pygame.image.load("Backgrounds/mainbackground.png").convert()
+main_screen = False
 
 # Settings screen surface
 settings_background_surf = pygame.image.load("Backgrounds/settingsbackground.png").convert()
@@ -55,17 +56,32 @@ class Button:
                 if self.clicked:
                     self.clicked = False
 
-# Buttons
-start_button = Button("Start", 150, 50, (200, 400))
-options_button = Button("Options", 150, 50, (450, 400))
-
 class Organism(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         crab1 = pygame.image.load("Crab/crab1.png").convert_alpha()
         self.image = crab1
-        self.rect = crab1.get_rect(center=(400, 400))
+        self.rect = crab1.get_rect(center=(400, 625))
 
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.rect.y -= 2
+        if keys[pygame.K_s]:
+            self.rect.y += 2
+        if keys[pygame.K_a]:
+            self.rect.x -= 2
+        if keys[pygame.K_d]:
+            self.rect.x += 2
+
+    def update(self):
+        self.player_input()
+
+# Buttons
+start_button_title = Button("Start", 150, 50, (200, 400))
+options_button_title = Button("Settings", 150, 50, (450, 400))
+back_button = Button("Back", 150, 50, (30, 30))
+options_button_main = Button("Settings", 150, 50, (620, 30))
 
 # Crab
 crab = pygame.sprite.GroupSingle()
@@ -82,19 +98,26 @@ while True:
     if title_screen == True and settings_screen == False:  # Displaying surfaces and rectangles on the screen
         screen.blit(title_background_surf, (0, 0))  # displays the title background image onto the screen
         screen.blit(title_name_surf, title_name_rect)  # displays the title name on top of the background image on the screen
-        start_button.draw()
-        options_button.draw()
-        if start_button.check_click():
-            title_screen = False
-        if options_button.check_click():
+        start_button_title.draw()
+        options_button_title.draw()
+        if start_button_title.check_click():
+            title_screen, main_screen = False, True
+        if options_button_title.check_click():
             title_screen, settings_screen = False, True
 
     elif settings_screen:
         screen.blit(settings_background_surf, (0, 0))
+        back_button.draw()
+        if back_button.check_click() == True:
+            title_screen, settings_screen = True, False
 
-    else:
+    elif main_screen:
         screen.blit(main_background_surf, (0, 0))
+        options_button_main.draw()
+        if options_button_main.check_click():
+            settings_screen, main_screen = True, False
         crab.draw(screen)
+        crab.update()
 
     pygame.display.update()  # updates the display
     clock.tick(60)  # Limits the runtime of the game to 60 ticks/frames per second
